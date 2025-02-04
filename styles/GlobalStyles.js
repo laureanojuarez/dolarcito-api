0c4e7bf950ca3a5c9852b180dbed60951e65b97a
Laureano <laureanojuarez69@gmail.com>
2025-02-03 23:16:00 -0300

    Agregue el favicon

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -1,14 +1,6 @@
 import {createGlobalStyle} from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-:root {
-  --main-primarycolor: #50a5c0;
-  --main-secondarycolor: #88c6d8;
-  --main-tertiarycolor: #b9dee8;
-  --main-bg-color: #274152;
-}
-
-
 * {
   padding: 0;
   margin: 0;
@@ -21,6 +13,4 @@ body {
   background: rgb(91,91,91);
   background: radial-gradient(circle, rgba(91,91,91,1) 0%, rgba(0,0,0,1) 200%);
 }
-
-
 `;
